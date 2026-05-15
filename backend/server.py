3519d9de33aeba629da477cc601203be00e160c2
Lauralmd07 <lauralmd07@gmail.com>
2026-05-15 14:30:35 -0300

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -20,7 +20,9 @@ ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
 # MongoDB connection
+# MongoDB connection
 mongo_url = os.environ['MONGO_URL']
+
 client = AsyncIOMotorClient(
     mongo_url,
     serverSelectionTimeoutMS=5000
