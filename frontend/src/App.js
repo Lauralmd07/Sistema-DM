8cfd823401b87caf5ebf801f09e0b3e16058ed0f
Lauralmd07 <lauralmd07@gmail.com>
2026-08-21 13:51:57 -0300

    feat: add clients workspace route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import { Dashboard } from './pages/Dashboard';
 import { Agenda } from './pages/Agenda';
 import { Processos } from './pages/Processos';
 import { Drive } from './pages/Drive';
+import { Clientes } from './pages/Clientes';
 import { FinanceiroPremium } from './pages/FinanceiroPremium';
 import './App.css';
 
@@ -18,52 +19,12 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-          
-          <Route
-            path="/agenda"
-            element={
-              <ProtectedRoute>
-                <Agenda />
-              </ProtectedRoute>
-            }
-          />
-          
-          <Route
-            path="/processos"
-            element={
-              <ProtectedRoute>
-                <Processos />
-              </ProtectedRoute>
-            }
-          />
-          
-          <Route
-            path="/drive"
-            element={
-              <ProtectedRoute>
-                <Drive />
-              </ProtectedRoute>
-            }
-          />
-          
-          <Route
-            path="/financeiro"
-            element={
-              <ProtectedRoute>
-                <FinanceiroPremium />
-              </ProtectedRoute>
-            }
-          />
-          
+          <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          <Route path="/agenda" element={<ProtectedRoute><Agenda /></ProtectedRoute>} />
+          <Route path="/processos" element={<ProtectedRoute><Processos /></ProtectedRoute>} />
+          <Route path="/drive" element={<ProtectedRoute><Drive /></ProtectedRoute>} />
+          <Route path="/clientes" element={<ProtectedRoute><Clientes /></ProtectedRoute>} />
+          <Route path="/financeiro" element={<ProtectedRoute><FinanceiroPremium /></ProtectedRoute>} />
           <Route path="/" element={<Navigate to="/dashboard" />} />
         </Routes>
       </BrowserRouter>
